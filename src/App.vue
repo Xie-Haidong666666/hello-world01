46cd420985e94d878005fa02002ddb91c3682222
xhd <xhd@168.com>
2021-03-03 10:13:41 +0800

    day002/withLym/Picture

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+   <img src="https://sh200821-gulionline-xhd.oss-cn-shanghai.aliyuncs.com/withLym/%E4%BE%84%E5%A5%B3%E5%92%8C%E5%8F%94%E5%8F%94.jpg"/>
   </div>
 </template>
 
